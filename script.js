dc9ea799f8e0aff921fe5517c532cc80e2d65e7e
Misha <misharexviashvili@yahoo.com>
2022-12-13 21:31:27 +0400

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+// Task 1
+document.querySelector("#button").addEventListener("click", () => {
+  document.querySelector(".hidden").classList.toggle("shown");
+});
+
+document.querySelector(".hidden").addEventListener("click", () => {
+  document.querySelector(".hidden").classList.remove("shown");
+});
