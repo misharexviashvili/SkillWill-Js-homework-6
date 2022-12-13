f3d403a7f996b4cf1b09a30c9a212e5f9de02d6f
Misha <misharexviashvili@yahoo.com>
2022-12-13 22:10:02 +0400

    third commit

--- script.js
+++ script.js
@@ -3,7 +3,28 @@
 document.querySelector("#button").addEventListener("click", () => {
   document.querySelector(".hidden").classList.toggle("shown");
 });
-
 document.querySelector(".hidden").addEventListener("click", () => {
   document.querySelector(".hidden").classList.remove("shown");
 });
+
+// Task 2
+let inputColor;
+document.querySelector(".btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  inputColor = document.querySelector(".input").value;
+  if (inputColor === "red") {
+    document.querySelector("body").style.backgroundColor = "red";
+  } else if (inputColor === "blue") {
+    document.querySelector("body").style.backgroundColor = "blue";
+  } else if (inputColor === "green") {
+    document.querySelector("body").style.backgroundColor = "green";
+  } else if (inputColor === "black") {
+    document.querySelector("body").style.backgroundColor = "black";
+  } else if (inputColor === "white") {
+    document.querySelector("body").style.backgroundColor = "white";
+  } else {
+    alert("Please type in red, blue, green, black or white");
+  }
+});
+
+// Task 3
